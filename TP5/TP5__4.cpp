ec083e2fb5ca6cc38275a6b1008f806136b88c55
Raphael-Nyphalem <nyphalem@pm.me>
2022-12-13 16:55:36 +0100

    TP5 4 mieu

--- TP5/TP5__4.cpp
+++ TP5/TP5__4.cpp
@@ -3,17 +3,17 @@ void  choixCouleur(int uneCouleur,int &r,int &g,int &b)
     {
         switch (uneCouleur)
         {
-            case 0
-                r,g,b = 255,0,0
+            case 0:
+                r,g,b = 255,0,0;
             break;
-            case 1
-                r,g,b = 0,255,0
+            case 1:
+                r,g,b = 0,255,0;
             break;
-            case 2
-                 r,g,b = 0,0,255
+            case 2:
+                 r,g,b = 0,0,255;
                  break;
-            case 3
-                r,g,b = 255,0,0
+            case 3:
+                r,g,b = 255,0,0;
                 break;
         
         default:
@@ -22,36 +22,40 @@ void  choixCouleur(int uneCouleur,int &r,int &g,int &b)
             
     }
 
-void allumerColonneCouleur(int unX1,unY1,unNbPixels,uneCouleur1)
+void allumerColonneCouleur(int unX1,int unY1,int unNbPixels,int uneCouleur1)
     {
-        int r,g,b
-        choixCouleur(uneCouleur1,r,g,b)
+        int r,g,b;
+        choixCouleur(uneCouleur1,r,g,b);
         for (int y = unY1; y<(unY1+unNbPixels); y++)
         {
-            senseSetRGBpixel(unX1,y,r, g, b)
+           // senseSetRGBpixel(unX1,y,r, g, b)
         }
     }
 
-void allumerLigneCouleur(int unX1,unY1,unNbPixels,uneCouleur1)
+void allumerLigneCouleur(int unX1,int unY1,int unNbPixels,int uneCouleur1)
     {
-        int r,g,b
-        choixCouleur(uneCouleur1,r,g,b)
+        int r,g,b;
+        choixCouleur(uneCouleur1,r,g,b);
         for (int x = unX1; x<(unX1+unNbPixels); x++)
         {
-            senseSetRGBpixel(x,unY1,r, g, b)
+            //senseSetRGBpixel(x,unY1,r, g, b)
         }
     }
 
-PROGRAMME Carré_en_couleur
-    declaration variables local
-        int x,y,i
-    {
-        POUR(i=0)QUAND(i<4)SUIVANT(i <-- i +1)
-            allumerColonneCouleur(i,i,(8-2*i),i)
-            allumerLigneCouleur(i,i,(8-2*i),i)
 
-            allumerColonneCouleur(7-i,i,(8-2*i),i)
-            allumerLigneCouleur(i,7-i,(8-2*i),i)
-        FIN_POUR
+
+int main(int argc, char const *argv[])
+{
+    for (int i = 0; i < 4; i++)
+    {
+        allumerColonneCouleur(i,i,(8-2*i),i);
+        allumerLigneCouleur(i,i,(8-2*i),i);
+
+        allumerColonneCouleur(7-i,i,(8-2*i),i);
+        allumerLigneCouleur(i,7-i,(8-2*i),i);
     }
-FIN_PROGRAMME
+    
+
+    return 0;
+}
+
