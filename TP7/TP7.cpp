13172e8a5949dd6f3099cf3bc3ddb77479face68
Raphael-Nyphalem <Nyphalem@pm.me>
2023-01-05 09:24:43 +0100

    TP7 exo 3

--- TP7/TP7.cpp
+++ TP7/TP7.cpp
@@ -46,7 +46,7 @@ const int MAX_NORME_ACCEL = 10;
 
 double calculNormeAccel(double xa,double ya, double za)
 {
-    return sqrt(xa*xa+ya*ya+za*za)
+    return sqrt(xa*xa+ya*ya+za*za);
 }
 
 double minNormeAccel(double min, double norme)
@@ -66,8 +66,8 @@ double minNormeAccel(double min, double norme)
 void ex2()
 {
     double x,y,z;
-    double norme, min;
-    min = MAX_NORME_ACCEL;
+    double normeAccel, normeMin;
+    normeMin = MAX_NORME_ACCEL;
 
     for (int i = 0; i < 100; i++)
     {
@@ -77,8 +77,10 @@ void ex2()
             << "x = " << x
             << " y = " << y
             << " z = " << z;
+
             normeAccel = calculNormeAccel(x, y, z);
             normeMin = minNormeAccel(normeAccel, normeMin);
+
             cout << "\t-> norme accélération : " << normeAccel << endl;
         }
         else
@@ -90,16 +92,58 @@ void ex2()
     
 }
 
-int conversionLineaire(int acc_norm)
+int conversionLineaire(double acc_norm)
 {
     int y_bits;
     y_bits = 7/0.9 * acc_norm - 7.9/0.9;
     return y_bits;
 }
 
+
+const int NB_MESURE;
+
 void ex3()
  {
+    double x,y,z;
+    double normeAccel, normeMin;
+    int tabVal[MAX];
+    int val;
 
+    for (int e = 0; e < MAX; e++)
+    {
+        normeMin = MAX_NORME_ACCEL;
+
+        for (int i = 0; i < NB_MESURE; i++)
+        {
+            
+            sleep_for(milliseconds(100));
+            if (senseGetAccelG(x, y, z)) {
+                
+                /*
+                cout << showpos << fixed << setprecision(6) 
+                << "x = " << x
+                << " y = " << y
+                << " z = " << z;
+                */
+
+                normeAccel = calculNormeAccel(x, y, z);
+                normeMin = minNormeAccel(normeAccel, normeMin);
+
+                cout << "\t-> norme accélération : " << normeAccel << endl;
+            }
+            else
+            {
+                cout << "Pas de mesure disponible." << endl;
+            }
+            cout << "La norme minimale d'accélération mesurée est : " << normeMin << endl;
+
+            val = conversionLineaire(normeMin)
+            tabVal[i]=val;
+        }
+        afficheHistogramme(tabVal);
+    }
+    
+    
  }
 
 
